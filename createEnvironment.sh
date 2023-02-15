99f4e7ec75bce1336b6b35a9d4374818d314e6e7
jroblesluna <jrobles@sistemas.com.pe>
2023-02-14 23:33:14 -0500

    FidelityCoin

--- createEnvironment.sh
+++ createEnvironment.sh
@@ -1,7 +1,7 @@
 npm install --save-dev webpack webpack-cli webpack-dev-server @webpack-cli/generators html-webpack-plugin workbox-webpack-plugin
 npm install --save-dev hardhat chai @nomicfoundation/hardhat-toolbox @nomicfoundation/hardhat-network-helpers @nomiclabs/hardhat-etherscan
-npm install --save-dev @openzeppelin/hardhat-upgrades @openzeppelin/contracts @openzeppelin/contracts-upgradeable
-npm install --save-dev dotenv
+npm install --save-dev @openzeppelin/hardhat-upgrades
+npm install dotenv @openzeppelin/contracts @openzeppelin/contracts-upgradeable
 npx hardhat init
 echo "\x1b[1;31m 🚨 Don't enable PWA 👀 \x1B[0m"
 npx webpack init
