fd29047975c7830cb41ec12da5a11f9cf7d1547d
jroblesluna <jrobles@sistemas.com.pe>
2023-02-14 20:09:52 -0500

    FidelityCoin

--- contracts/FidelityCoin.sol
+++ contracts/FidelityCoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract FideliCoin is ERC20, ERC20Burnable, AccessControl  {
+contract FidelityCoin is ERC20, ERC20Burnable, AccessControl  {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     
     uint256 private _expirationPeriod = 60; //Starts With 60 seconds for expiration
