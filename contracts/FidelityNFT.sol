4e022e5f78d3a9d4cd965c20b8e978df622c3db2
esanch3z <esanch3z@users.noreply.github.com>
2023-02-17 21:49:39 -0500

    Update FidelityNFT.sol

--- contracts/FidelityNFT.sol
+++ contracts/FidelityNFT.sol
@@ -44,7 +44,6 @@ contract FidelityNFT is
     using Strings for uint256;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     string ipfsCID;
     uint256 lastNftId;
